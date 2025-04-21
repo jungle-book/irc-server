96747a2d8b79d531071fe4f613af6355b3102528
rohit20001221 <rohit20001221@gmail.com>
2025-04-22 01:42:37 +0530

    .

--- write_to_client.go
+++ write_to_client.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 func writeToClient(client *Client) {
 	for msg := range client.ch {
-		log.Println(msg)
-
 		fmt.Fprintf(client.conn, "%s\r\n", msg)
 	}
 }
